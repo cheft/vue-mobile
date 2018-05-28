b5de1aa789b79f51f113e9cb1f05fdfc8a213224
cheft <m@cheft.cn>
2018-05-28 21:12:31 +0800

    init

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Pris from './views/Pris.vue';
+import Transaction from './views/Transaction.vue';
+import Profile from './views/Profile.vue';
 import About from './views/About.vue';
 
 Vue.use(Router);
@@ -11,6 +14,21 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/pris',
+      name: 'pris',
+      component: Pris,
+    },
+    {
+      path: '/transaction',
+      name: 'transaction',
+      component: Transaction,
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: Profile,
     },
     {
       path: '/about',
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Button>按钮测试</Button>
   </div>
 </template>
+
+<script>
+  import Button from 'vant/lib/button';
+  import 'vant/lib/vant-css/base.css';
+  import 'vant/lib/vant-css/button.css';
+
+  export default {
+    name: 'about',
+    components: {
+      Button,
+    },
+  };
+</script>
